a400a01419d4c1dbb3b6d11269d6612ef2c08e85
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-03-12 20:02:48 +0100

    Added model and form for shortening urls

--- WebDevAcademy.UrlShortener/WebDevAcademy.UrlShortener/Controllers/ShortenController.cs
+++ WebDevAcademy.UrlShortener/WebDevAcademy.UrlShortener/Controllers/ShortenController.cs
@@ -4,9 +4,16 @@ namespace WebDevAcademy.UrlShortener.Controllers
 {
     public class ShortenController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShortenUrl()
+        {
+            return Index();
         }
     }
 }
---
+++ WebDevAcademy.UrlShortener/WebDevAcademy.UrlShortener/Models/ShortenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebDevAcademy.UrlShortener.Models
+{
+    public class ShortenModel
+    {
+        public int Id { get; set; }
+        public string LongUrl { get; set; }
+        public string ShortUrl { get; set; }
+    }
+}
