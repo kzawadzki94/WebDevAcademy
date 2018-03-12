51ae4460deff95983327e05f9101f8f3f6765cca
Krzysztof Zawadzki <krzysztof.zawadzki94@gmail.com>
2018-03-12 19:37:13 +0100

    Initial commit

---
+++ WebDevAcademy.UrlShortener/WebDevAcademy.UrlShortener/Controllers/ShortenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevAcademy.UrlShortener.Controllers
+{
+    public class ShortenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
